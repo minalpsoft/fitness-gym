f4e2ca4d6e26afed923f40ea76f4e82e8eea8dbf
minalpsoft <minal@psoftsolutions@.in>
2026-01-27 15:34:53 +0530

    update

--- app.config.js
+++ app.config.js
@@ -4,6 +4,9 @@ export default {
   expo: {
     name: "fitness-gym",
     slug: "fitness-gym",
+      android: {
+      package: "com.minalpsoft.fitnessgym",
+    },
      extra: {
       eas: {
         projectId: "3997e771-3134-48be-b227-d294a624b596",
